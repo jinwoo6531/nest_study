9a1ebbc0dcadddb74903378ae93d6d111dc9464d
jinwoo6531 <jintakk6531@gmail.com>
2021-01-26 18:09:35 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
       transform: true, //데이터가 넘어올떄 자동으로 타입을 변환시켜준다(원래 string)
     }),
   );
-  await app.listen(3000);
+  await app.listen(3000); //혹시
 }
 bootstrap();
